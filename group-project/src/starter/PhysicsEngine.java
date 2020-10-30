bb331979276aac664b8e0feb494db3b488381034
Kaung Khant Pyae Sone <k_kaung@u.pacific.edu>
2020-10-30 16:50:32 -0700

    Fixed the SomePane error.

--- group-project/src/starter/PhysicsEngine.java
+++ group-project/src/starter/PhysicsEngine.java
@@ -1,7 +1,24 @@
 package starter;
-
-// Empty Class
+import acm.graphics.*;
+import java.util.*;
+// Still non functional
 
 public class PhysicsEngine {
-
+	private int mainObject; // Index to the main object, which will be controlled by keyboard input
+	private ArrayList<GObject> objects; // Will be changed to Moveable class later, currently GObject for testing.
+	
+	
+	
+	/**
+	 * The update() function is where all the physics and movements will be calculated. This function must
+	 * be called by a game loop or a timer, which update the calculations every * seconds. The physics
+	 * engine is also responsible for the movement of the Moveable class.
+	 */
+	public void update() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
 }
