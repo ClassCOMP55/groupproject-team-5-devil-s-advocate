4db7053f1e8d7de3475685e265480a0c292e9b00
Ade Sampson <a_sampson2@u.pacific.edu>
2020-10-30 11:24:31 -0700

    Added two new classes, Immovable & Moveable

--- group-project/src/starter/Immovable.java
+++ group-project/src/starter/Immovable.java
@@ -1,7 +1,7 @@
 package starter;
 
 public class Immovable {
-
+// This is going to be where we put the visual representation of the Pipe, Platform, and ground
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- group-project/src/starter/Moveable.java
+++ group-project/src/starter/Moveable.java
@@ -1,7 +1,7 @@
 package starter;
 
 public class Moveable {
-
+	// This is going to be where we put the Mario and Goomba
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
