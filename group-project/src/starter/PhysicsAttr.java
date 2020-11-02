6ba5586f63aa2ff7b82233d2ad7a6173d6863159
Kaung Khant Pyae Sone <k_kaung@u.pacific.edu>
2020-11-02 15:50:15 -0800

    Removed associatedObject variable. Seems to be unnecessary

--- group-project/src/starter/PhysicsAttr.java
+++ group-project/src/starter/PhysicsAttr.java
@@ -5,7 +5,6 @@ public class PhysicsAttr {
     public double xVel, yVel; // Velocities for X and Y axes
     public double xVelMax, yVelMax; // Velocity limits for the X and Y axes
     public String xDirection, yDirection; // Direction in which the character is traveling. Used by the physicsengine to decide which way to move the character.
-    public GObject associatedObject; // A pointer to the object that the attributes are associated with.
     public boolean constLinearMovement; // Constant linear movement, for moving enemies like goombas at a constant speed.
 }
 
