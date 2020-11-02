1c46d158ff4747d28815126341d5d76d97ee78d8
Kaung Khant Pyae Sone <k_kaung@u.pacific.edu>
2020-11-02 15:48:53 -0800

    Removed acceleration and deceleration variables for both axes.
    Acceleration and deceleration will be hardcoded as formulas in
    PhysicsEngine

--- group-project/src/starter/PhysicsAttr.java
+++ group-project/src/starter/PhysicsAttr.java
@@ -4,7 +4,6 @@ import acm.graphics.*;
 public class PhysicsAttr {
     public double xVel, yVel; // Velocities for X and Y axes
     public double xVelMax, yVelMax; // Velocity limits for the X and Y axes
-    public double xAccel, xDecel, yAccel, yDecel; // Acceleration and deceleration for x and y axes, can be used to implement gravity
     public String xDirection, yDirection; // Direction in which the character is traveling. Used by the physicsengine to decide which way to move the character.
     public GObject associatedObject; // A pointer to the object that the attributes are associated with.
     public boolean constLinearMovement; // Constant linear movement, for moving enemies like goombas at a constant speed.
