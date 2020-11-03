10557169bee6201eb006a221887b880faa58f6cc
muradhaider <m_haider3@u.pacific.edu>
2020-11-02 20:48:36 -0800

    had to re do win screen, very similar code to the deadscreen. however, this will display after the player passes the level. Also added the win screen png which we decided to make the same background as the dead screen but show mario and display win message.

---
+++ group-project/src/starter/WinScreen.java
@@ -0,0 +1,67 @@
+package starter;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+
+public class WinScreen {
+	private MainApplication program; // you will use program to get access to
+	//we will change these images									// all of the GraphicsProgram calls
+	private GImage WinScreen;
+	private GLabel Mario_Win;
+	
+	private GButton playAgainButton;// keep buttons
+	private GButton quitButton;
+
+
+	
+	public WinScreen(MainApplication app) {
+		super();
+		program = app;
+		//change images
+		
+		WinScreen = new GImage("background/DeadScreen.png", 0, 0);
+		
+		Mario_Win = new GLabel("You have passed the level!",150,50);
+		Mario_Win.setColor(Color.RED);
+		
+		playAgainButton = new GButton("Play Again", 250, 325, 300, 75);
+		playAgainButton.setFillColor(Color.RED);
+		
+		quitButton = new GButton("Quit", 250, 425, 300, 75);
+		quitButton.setFillColor(Color.RED);
+		
+	}
+	
+		public void showContents() {
+			// TODO Auto-generated method stub
+			//Change images 
+			program.add(WinScreen);
+			program.add(Mario_Win);
+			
+			
+			program.add(playAgainButton);
+			program.add(quitButton);
+		}
+
+		public void hideContents() {
+			// TODO Auto-generated method stub
+			
+
+			program.remove(playAgainButton);
+			program.remove(quitButton);
+
+		}
+
+
+		public void mousePressed(MouseEvent e) {
+			GObject obj = program.getElementAt(e.getX(), e.getY());
+			if (obj == quitButton) {
+				program.switchToInstructions();
+			}
+		}
+	}
+
