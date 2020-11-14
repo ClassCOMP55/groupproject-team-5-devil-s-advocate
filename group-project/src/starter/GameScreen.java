9295d17b18d03a6544a966d122be27cc3a74a260
Michelle <m_vu12@u.pacific.edu>
2020-11-13 17:32:54 -0800

    Created the GameScreen file to implement the .tmx file

---
+++ group-project/src/starter/GameScreen.java
@@ -0,0 +1,16 @@
+package starter;
+
+public class GameScreen {
+   
+    //private TmxMapLoader maploader;
+    //private TiledMap map;
+    //private OrthogonalTiledMapRenderer renderer;
+	
+	//maploader = new TmxMapLoader();
+    //map = maploader.load("level1.tmx");
+    //renderer = new OrthogonalTiledMapRenderer(map, 1  / MarioBros.PPM);
+
+    //initially set our gamcam to be centered correctly at the start of of map
+    //gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+}
