7e5143cf8c8a5762befc4393c7358b55a26c988b
Kaung Khant Pyae Sone <k_kaung@u.pacific.edu>
2020-11-17 19:06:35 -0800

    Made TileTestGraphics.java scroll through the level

--- group-project/src/starter/TestTileGraphics.java
+++ group-project/src/starter/TestTileGraphics.java
@@ -4,19 +4,49 @@ import acm.program.GraphicsProgram;
 import acm.graphics.*;
 
 public class TestTileGraphics extends GraphicsProgram {
-	
 	TMXLevelReader reader;
 	private final int WINDOW_SIZE = 700;
+	GCompound compound = new GCompound();
 	
 	public void init() {
 		setSize(WINDOW_SIZE,WINDOW_SIZE);
 		requestFocus();
 		reader = new TMXLevelReader("/levels/test/Test_Level.tmx", "/SpriteSheet/tileset.png", WINDOW_SIZE);
+		for (GImage image : reader.allGImages) {
+			compound.add(image);
+		}
+		add(compound);
 	}
 
 	public void run() {
-		for (GImage image : reader.allGImages) {
-			add(image);
-		}
+		gameLoop();
+	}
+	
+	private void gameLoop() {
+	    long now;
+	    long updateTime;
+	    long wait;
+
+	    final int TARGET_FPS = 60;
+	    final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+
+	    while (true) {
+	        now = System.nanoTime();
+
+	        updateTime = System.nanoTime() - now;
+	        wait = (OPTIMAL_TIME - updateTime) / 1000000;
+	        
+	        if (-compound.getX() == compound.getWidth() - WINDOW_SIZE) {
+	        	
+	        } else {
+	        	compound.move(-1,0);
+	        }
+	        
+	        try {
+	            Thread.sleep(wait);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
 }
