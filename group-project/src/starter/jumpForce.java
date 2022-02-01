52f96d03871eabf3d1ffa0de82c3ba221912a55f
Michelle <m_vu12@u.pacific.edu>
2022-01-31 23:10:08 -0800

    Alternative way of reading in the jumpForce.txt using FileReader

--- group-project/src/starter/jumpForce.java
+++ group-project/src/starter/jumpForce.java
@@ -1,12 +1,36 @@
 package starter;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+* @author www.codejava.net
+*/
+public class jumpForce {
+
+   public static void main(String[] args) {
+       try {
+           FileReader reader = new FileReader("jumpForce.txt");
+           int character;
+
+           while ((character = reader.read()) != -1) {
+               System.out.print((char) character);
+           }
+           reader.close();
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+
+}
+
+/*
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-// PhysicsEngine line 151 
-	// Adjust the mainEntity.yVel 
-	// Attempt to read in value from jumpForce.txt to then place as the
+
 	// Reference: https://www.baeldung.com/java-file-to-arraylist
 
 public class jumpForce {
@@ -14,12 +38,17 @@ public class jumpForce {
 	//ArrayList<String> force = new ArrayList<>();
 
 	
-	/*
+	
 	try (BufferedReader br = new BufferedReader(new FileReader("jumpForce.txt"))) { 
 	    while (br.ready()) {
 	    	force.add(br.readLine());
 	    }
 	}
-	}*/
+	}
 }
+*/
+	// PhysicsEngine line 151 
+	// Adjust the mainEntity.yVel 
+	// Attempt to read in value from jumpForce.txt to then place as the
+
 
