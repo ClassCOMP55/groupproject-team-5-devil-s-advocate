f426a685e2794d544d8846386e52ee06ecfc16fe
Michelle <m_vu12@u.pacific.edu>
2022-01-31 22:36:19 -0800

    BufferReader for JumpForce.txt

--- group-project/src/starter/jumpForce.java
+++ group-project/src/starter/jumpForce.java
@@ -4,19 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-
-public class jumpForce {
-	
-	ArrayList<String> force = new ArrayList<>();
-
-	// PhysicsEngine line 151 
+// PhysicsEngine line 151 
 	// Adjust the mainEntity.yVel 
 	// Attempt to read in value from jumpForce.txt to then place as the
 	// Reference: https://www.baeldung.com/java-file-to-arraylist
 
+public class jumpForce {
+	
+	//ArrayList<String> force = new ArrayList<>();
+
+	
+	/*
 	try (BufferedReader br = new BufferedReader(new FileReader("jumpForce.txt"))) { 
 	    while (br.ready()) {
 	    	force.add(br.readLine());
 	    }
 	}
+	}*/
 }
+
